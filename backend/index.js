53867e5024d7e60261d4a208199564cafa92792a
Tarun Choudhary <111572130+tarun-8067@users.noreply.github.com>
2024-07-16 11:23:02 +0530

    010

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
+
+app.use(cors()); // Allow all origins
+app.use(express.json());
 const PORT = process.env.PORT || 3001;
 
-// app.use("/",(req,res) => {
-//     res.send("server is running");
-//   })
+app.get("/",(req,res) => {
+    res.send("server is running");
+  })
 
 
-  app.use("/sendEmail",(req, res) => {
+  app.get("/sendEmail",(req, res) => {
     res.send("in Send -email");
   })
 
