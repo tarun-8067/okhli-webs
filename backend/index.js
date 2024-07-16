b53484819e6d7fcb02c83bf458f940da08e49af5
Tarun Choudhary <111572130+tarun-8067@users.noreply.github.com>
2024-07-16 10:49:20 +0530

    001

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+
+app.use("/",(req,res) => {
+  res.send("server is running");
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
